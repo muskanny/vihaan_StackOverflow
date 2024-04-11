baf1f12d2613ca44700277fb89aea9cac5aa1b96
Srishti-Chugh <66906957+Srishti-Chugh@users.noreply.github.com>
2024-04-11 18:48:49 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,13 +1,9 @@
 function showChooseMealForm() {
     var chooseMealForm = document.getElementById("chooseMealForm");
-    var customizeMealForm = document.getElementById("customizeMealForm");
     chooseMealForm.style.display = "block";
-    customizeMealForm.style.display = "none";
 }
 
 function showCustomizeMealForm() {
-    var chooseMealForm = document.getElementById("chooseMealForm");
     var customizeMealForm = document.getElementById("customizeMealForm");
-    chooseMealForm.style.display = "none";
     customizeMealForm.style.display = "block";
 }
