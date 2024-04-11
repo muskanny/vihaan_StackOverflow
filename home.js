cb61b782332845c88ef4e7da4a31ab8fe7360387
Srishti-Chugh <66906957+Srishti-Chugh@users.noreply.github.com>
2024-04-11 19:22:28 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,10 +1,10 @@
 window.addEventListener('scroll', () => {
-    const ship = document.querySelector('.ship');
+    const meals = document.querySelector('.meals');
     const text = document.querySelector('.text');
     const scrolled = window.scrollY;
   
     // Adjust the speed factor as needed to achieve the desired parallax effect
-    ship.style.top = `${50 - scrolled * 0.1}%`; 
+    meals.style.top = `${50 - scrolled * 0.1}%`; 
     text.style.top = `${20 - scrolled * 0.05}%`; 
   });
   
